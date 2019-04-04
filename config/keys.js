27f1765712820eb45c1e581095a5f5f34c4b7c16
Anh Nguyen Viet (DBS - DFC) <anhnv22@vpbank.com.vn>
2019-04-04 14:03:53 +0700

    update code

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PORT: 3000,
+  PORT: process.env.PORT || 3000,
   DB_LOCAL: 'mongodb://localhost:27017/todoManagementUsingExpress',
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hello Vanh' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Hello Vanh 2' })
 });
 
-module.exports = router;
+module.exports = router
