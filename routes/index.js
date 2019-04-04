05a44353c2236ebaf4958c9f3859925de38a4a93
Anh Nguyen Viet (DBS - DFC) <anhnv22@vpbank.com.vn>
2019-04-04 14:12:45 +0700

    Ahihi

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello Vanh 2' })
+  res.render('index', { title: 'Hello Vanh' })
 });
 
 module.exports = router
